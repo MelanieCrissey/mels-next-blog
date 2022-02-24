a103cc993f472da0fdc9ce1e4ccb084a962721dd
Melanie Crissey <melaniecrissey@gmail.com>
2022-02-24 10:30:25 -0500

    Making this thing my own

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Melanie Crissey';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'My Next.js Blog';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'NO rights reserved. Muhahaha';
 
   return {
     name,
